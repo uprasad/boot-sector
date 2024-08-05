4ca080652f2e009a0d1a4c186bbddb1e1f42c706
Udbhav Prasad <udbhav523@gmail.com>
2024-08-04 18:58:27 -0700

    Print characters to the screen

--- boot.asm
+++ boot.asm
@@ -1,7 +1,20 @@
-; A simple boot sector program that loops forever.
+; A simple boot sector that prints a message to the screen using a BIOS
+; routine.
 
-loop:
-  jmp loop
+mov ah, 0x0e
+
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $
 
 ; $ is the address of the current instruction.
 ; $$ is the address of the first instruction in this section.
@@ -9,4 +22,6 @@ loop:
 times 510-($-$$) db 0
 
 ; dw is 'direct word'
+; Last two words are the magic number to let the BIOS know that this is a boot
+; sector.
 dw 0xaa55
