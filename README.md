6d10f3b978cccded81d3635e80b7af8eeddfdf8e
Udbhav Prasad <udbhav523@gmail.com>
2024-08-08 22:53:37 -0700

    Create boot image that demonstrates addressing

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Command to generate boot disk image from assembly file
 ```bash
 nasm boot.asm -f bin -o boot_nasm.bin 
 ```
+
+Command to view the hexdump of a binary
+```bash
+xxd boot_nasm.bin
+```
---
+++ boot_addressing.asm
@@ -0,0 +1,23 @@
+; A simple boot sector program that demonstrates addressing.
+
+mov ah, 0x0e
+
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
+int 0x10
+
+jmp $
+
+the_secret:
+  db 'X'
+
+; $ is the address of the current instruction.
+; $$ is the address of the first instruction in this section.
+; db or 'direct byte' places a byte at this location
+times 510-($-$$) db 0
+
+; dw is 'direct word'
+; Last two words are the magic number to let the BIOS know that this is a boot
+; sector.
+dw 0xaa55
